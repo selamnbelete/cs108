5616e6b460999779c606227f68c6028a9dd39d8a
selam belete <selam.belete@wallawalla.edu>
2026-04-27 13:51:41 -0700

    m

--- Cs108_project/main.py
+++ Cs108_project/main.py
@@ -1,9 +1,9 @@
 import pygame
 pygame.init()
 
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Garden Simulation")
 
 # Create a plant instance
